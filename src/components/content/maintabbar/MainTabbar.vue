7f8a369ed7c686a5d97a3ed626e2df32bcf3b5c6
F-Yanyu <F-Yanyu@outlook.com>
2021-09-23 00:26:21 +0800

    'fu'

--- src/components/content/maintabbar/MainTabbar.vue
+++ src/components/content/maintabbar/MainTabbar.vue
@@ -1,22 +1,22 @@
 <template>
   <div>
     <tabbar>
-      <tabbar-item path="/home" activeColor="blue">
+      <tabbar-item path="/home">
         <img slot="item-icon" src="~assets/img/tabbar/black/home.png" alt="">
         <img slot="item-icon-isactive" src="~assets/img/tabbar/red/home.png" alt="">
         <div slot="item-text">首页</div>
       </tabbar-item>
-      <tabbar-item path="/classification" activeColor="blue">
+      <tabbar-item path="/classification">
         <img slot="item-icon" src="~assets/img/tabbar/black/classification.png" alt="">
         <img slot="item-icon-isactive" src="~assets/img/tabbar/red/classification.png" alt="">
         <div slot="item-text">分类</div>
       </tabbar-item>
-      <tabbar-item path="/shoppingcart" activeColor="blue">
+      <tabbar-item path="/shoppingcart">
         <img slot="item-icon" src="~assets/img/tabbar/black/gouwuche.png" alt="">
         <img slot="item-icon-isactive" src="~assets/img/tabbar/red/gouwuche.png" alt="">
         <div slot="item-text">购物车</div>
       </tabbar-item>
-      <tabbar-item path="/profile" activeColor="blue">
+      <tabbar-item path="/profile">
         <img slot="item-icon" src="~assets/img/tabbar/black/wode.png" alt="">
         <img slot="item-icon-isactive" src="~assets/img/tabbar/red/wode.png" alt="">
         <div slot="item-text">我的</div>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,14 +1,37 @@
 <template>
-  <div>首页</div>
+  <div id="home">
+    <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+  </div>
 </template>
 <script>
+import NavBar from "components/common/nav-bar/NavBar.vue";
+import { getHomeMultidata } from "network/home";
+
 export default {
-  data(){
-    return{
-    }
+  data() {
+    return {
+      banners: [],
+      recommends: []
+    };
   },
-  name: 'Home'
-}
+  name: "Home",
+  components: {
+    NavBar
+  },
+  created() {
+    getHomeMultidata().then(result => {
+      // console.log(result);
+      // 因为函数有调用栈，使用完会被内存回收掉，所以，要提出来到data中，
+      // 这样本应result指向的数组，在result弹出函数栈后，不会因为没有东西执行而被回收
+      this.banners = result.data.banner.list;
+      this.recommends = result.data.recommend.list;
+    });
+  }
+};
 </script>
-<style>
+<style scope>
+.home-nav {
+  background-color: var(--color-tint);
+  color: #fff;
+}
 </style>
