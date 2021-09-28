0050c54af3c7e1a3ed17e4b8b590185836e01290
F-Yanyu <F-Yanyu@outlook.com>
2021-09-28 23:24:41 +0800

    axios

--- src/network/axios.js
+++ src/network/axios.js
@@ -5,9 +5,9 @@ export default function axios(option) {
 	return new Promise((resolve, reject) => {
 		// 1.创建axios的实例
 		const instance = originAxios.create({
-			baseURL: 'http://123.207.32.32:8000',
-			timeout: 5000
-		});
+		baseURL: 'http://123.207.32.32:8000',
+		timeout: 5000
+	});
 
 		// 配置请求和响应拦截
 		instance.interceptors.request.use(config => {
